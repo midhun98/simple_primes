181d5be58cad8236c8e8447633501839b1dc36b7
Midhun <midhunskani@gmail.com>
2024-09-26 19:52:30 +0530

    Create the UI and backend

--- primes/apps.py
+++ primes/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PrimesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'primes'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "primes"
--- primes/views.py
+++ primes/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+
+def primes(number):
+    prime_list = []
+    if number < 2:
+        return []
+    for n in range(2, number + 1):
+        for i in range(2, n):
+            if n % i == 0:
+                break
+        else:
+            prime_list.append(n)
+    return prime_list
+
+
+# Input page view
+def input_page(request):
+    return render(request, "input_page.html")
+
+
+# Output page view
+def output_page(request):
+    if request.method == "POST":
+        number = int(request.POST["number"])
+        prime_list = primes(number)
+        return render(
+            request, "output_page.html", {"primes": prime_list, "number": number}
+        )
+    return HttpResponseRedirect(reverse("input_page"))
